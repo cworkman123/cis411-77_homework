6d74f1b00c151395bc95ce9fbab7c58369e5a9b4
cworkman123 <w0work02@louisville.edu>
2014-02-26 14:45:32 -0500

    HW PART 8
    adding validations to the model

--- homework5/MvcMovie/MvcMovie/Models/Movie.cs
+++ homework5/MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
-        public  string Rating { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+
+        //public int ID { get; set; }
+        //public string Title { get; set; }
+        //public DateTime ReleaseDate { get; set; }
+        //public string Genre { get; set; }
+        //public decimal Price { get; set; }
+        //public  string Rating { get; set; }
     }
 
 
