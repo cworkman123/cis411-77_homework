50234564b304e32580ec193b20484a064dee7243
cworkman123 <w0work02@louisville.edu>
2014-03-18 16:59:40 -0400

    HW 6
    Create Model and Add Search Function

---
+++ homework6/PetStore/PetStore/Controllers/ManageController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetStore.Models;
+
+namespace PetStore.Controllers
+{
+    public class ManageController : Controller
+    {
+        private ManageDBContext db = new ManageDBContext();
+
+        // GET: /Manage/
+        public ActionResult Index(string searchString)
+        {
+            var manages = from m in db.Manages
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                manages = manages.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(manages);
+        }
+
+        // GET: /Manage/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manage manage = db.Manages.Find(id);
+            if (manage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manage);
+        }
+
+        // GET: /Manage/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Manage/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,Description,DateReceived,Quantity,Price")] Manage manage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Manages.Add(manage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(manage);
+        }
+
+        // GET: /Manage/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manage manage = db.Manages.Find(id);
+            if (manage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manage);
+        }
+
+        // POST: /Manage/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,Description,DateReceived,Quantity,Price")] Manage manage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(manage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(manage);
+        }
+
+        // GET: /Manage/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manage manage = db.Manages.Find(id);
+            if (manage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manage);
+        }
+
+        // POST: /Manage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Manage manage = db.Manages.Find(id);
+            db.Manages.Remove(manage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ homework6/PetStore/PetStore/Models/Manage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PetStore.Models
+{
+    public class Manage
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+
+    public class ManageDBContext : DbContext
+    {
+        public DbSet<Manage> Manages { get; set; }
+    }
+}
