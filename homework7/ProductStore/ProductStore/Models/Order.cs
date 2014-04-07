f9df5b88b5a22a46009a064d517f290cbda95dbd
cworkman123 <w0work02@louisville.edu>
2014-04-07 14:49:08 -0400

    HW7
    Creating the Domain Models

---
+++ homework7/ProductStore/ProductStore/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+
+        //Navigation Property
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
