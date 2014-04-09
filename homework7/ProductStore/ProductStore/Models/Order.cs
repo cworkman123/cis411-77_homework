3dfe1a323da1a52a905acb224dd376a8ad355c7f
cworkman123 <w0work02@louisville.edu>
2014-04-09 14:45:18 -0400

    HW7
    Creating an Admin Controller

--- homework7/ProductStore/ProductStore/Models/Order.cs
+++ homework7/ProductStore/ProductStore/Models/Order.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ProductStore.Models
+﻿namespace ProductStore.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class Order
     {
+        public int Id { get; set; }
+        [Required]
+        public string Customer { get; set; }
+
+        // Navigation property
+        public ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
